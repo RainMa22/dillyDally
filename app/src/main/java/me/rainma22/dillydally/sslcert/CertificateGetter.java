435a9847f62613672de6528fb7b445f8185ade7a
RainMa22 <ruiruima22@gmail.com>
2026-08-23 21:30:51 -0700

    remove unnessary imports

--- app/src/main/java/me/rainma22/dillydally/sslcert/CertificateGetter.java
+++ app/src/main/java/me/rainma22/dillydally/sslcert/CertificateGetter.java
@@ -1,11 +1,8 @@
 package me.rainma22.dillydally.sslcert;
 
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -17,56 +14,12 @@ import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.PKCS12Attribute;
-import java.security.SecureRandom;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.time.Instant;
-import java.time.LocalTime;
-import java.util.Date;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
-import java.util.random.RandomGenerator;
-
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
-import org.bouncycastle.asn1.x509.Time;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.X509v3CertificateBuilder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
-import org.bouncycastle.crypto.CipherParameters;
-import org.bouncycastle.crypto.engines.AESEngine;
-import org.bouncycastle.crypto.engines.AESLightEngine;
-import org.bouncycastle.crypto.params.KeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithIV;
-import org.bouncycastle.crypto.util.AlgorithmIdentifierFactory;
-import org.bouncycastle.jcajce.BCFKSLoadStoreParameter.SignatureAlgorithm;
-import org.bouncycastle.jcajce.provider.asymmetric.rsa.X931SignatureSpi.SHA512WithRSAEncryption;
-import org.bouncycastle.jcajce.provider.asymmetric.x509.CertificateFactory;
-import org.bouncycastle.jcajce.provider.symmetric.AES;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.jce.provider.PEMUtil;
-import org.bouncycastle.openssl.PEMDecryptor;
-import org.bouncycastle.openssl.PEMDecryptorProvider;
-import org.bouncycastle.openssl.PEMEncryptedKeyPair;
 import org.bouncycastle.openssl.PEMEncryptor;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
-import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
 import org.bouncycastle.openssl.jcajce.JcePEMEncryptorBuilder;
-import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-import org.bouncycastle.util.Arrays;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemObjectGenerator;
-import org.bouncycastle.util.io.pem.PemWriter;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -140,23 +93,13 @@ public class CertificateGetter {
                 KeyPair acmeKeyPair = certGetter.getKeyPair();
                 Path pemPath = Path.of(sslConf.getPathToACMEPEM());
                 Files.createDirectories(pemPath.getParent());
-                
+
                 try (JcaPEMWriter writer = new JcaPEMWriter(
                                 new FileWriter(Path.of(sslConf.getPathToACMEPEM()).toFile()))) {
                         PEMEncryptor encryptor = new JcePEMEncryptorBuilder("AES-256-CBC")
                                         .build(sslConf.getAcmePassword().toCharArray());
                         writer.writeObject(acmeKeyPair, encryptor);
                 }
-                // try (PEMParser parser = new PEMParser(new FileReader("acme.pem"))) {
-
-                // var decryptorProvider = new JcePEMDecryptorProviderBuilder()
-                // .build(httpConf.getAcmePassword().toCharArray());
-                // JcaPEMKeyConverter converter = new JcaPEMKeyConverter()
-                // .setProvider(new BouncyCastleProvider());
-                // var pemKp = (PEMEncryptedKeyPair) parser.readObject();
-                // PEMKeyPair pKp = pemKp.decryptKeyPair(decryptorProvider);
-                // KeyPair kp = converter.getKeyPair(pKp);
-                // }
 
                 if (certGetter.currState instanceof CompletedState) {
                         CompletedState state = (CompletedState) certGetter.currState;
