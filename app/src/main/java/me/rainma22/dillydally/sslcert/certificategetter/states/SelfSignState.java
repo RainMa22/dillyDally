9994d9d692ef74dc3ff72e19e3874892497983c4
RainMa22 <ruiruima22@gmail.com>
2026-09-08 16:47:16 -0700

    self-sign: add multiple name support

--- app/src/main/java/me/rainma22/dillydally/sslcert/certificategetter/states/SelfSignState.java
+++ app/src/main/java/me/rainma22/dillydally/sslcert/certificategetter/states/SelfSignState.java
@@ -2,9 +2,8 @@ package me.rainma22.dillydally.sslcert.certificategetter.states;
 
 import me.rainma22.dillydally.sslcert.certificategetter.CertificateGetterContext;
 import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.BasicConstraints;
-import org.bouncycastle.asn1.x509.Extension;
-import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.asn1.x509.*;
+import org.bouncycastle.asn1.x509.sigi.NameOrPseudonym;
 import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
@@ -30,7 +29,8 @@ public class SelfSignState implements CertificateGetterState {
             gen.initialize(2048);
             var sslKeyPair = gen.genKeyPair();
             ctx.setSslKeyPair(sslKeyPair);
-            var name = new X500Name("CN=localhost");
+            var domains = ctx.getConf().getDomains();
+            var name = new X500Name(String.format("CN=%s", ctx.getConf().getDomains().get(0)));
             var certBuilder = new JcaX509v3CertificateBuilder(name, BigInteger.TWO, Date.from(Instant.now()),
                     Date.from(Instant.now().plus(Duration.ofDays(3650))), name, sslKeyPair.getPublic())
                     .addExtension(
@@ -42,6 +42,13 @@ public class SelfSignState implements CertificateGetterState {
                             Extension.keyUsage,
                             true,
                             new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyEncipherment)
+                    )
+                    .addExtension(
+                            Extension.subjectAlternativeName,
+                            false,
+                            new GeneralNames(domains.stream()
+                                    .map(str -> new GeneralName(GeneralName.dNSName,str))
+                                    .toArray(GeneralName[]::new))
                     );
             ContentSigner signer = new JcaContentSignerBuilder("SHA256withRSA")
                     .setProvider("BC")
